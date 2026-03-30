1c67f2c4f811a4e720d0378d6e18e7a6bf5b4137
jawfbs <jake@fbsdata.com>
2026-03-30 15:38:57 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -593,6 +593,7 @@ export default function Home() {
                     onClick={() => setDataFilter("with-data")}
                     title="Show fields with data"
                   >
+                       }
                     <svg width="10" height="10" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
                       <polyline points="20 6 9 17 4 12" />
                     </svg>
